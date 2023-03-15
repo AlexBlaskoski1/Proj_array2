cb8a7466f2b26be806b5c77b4e7051dbf47743ce
ALEX BLASKOSKI <alex_blaskoski@estudante.sesisenai.org.br>
2023-03-15 09:39:50 -0300

    incluindo innerhmtl para imprimir na tela

--- vetor.js
+++ vetor.js
@@ -1,14 +1,15 @@
 let valor_cidade = []
+let res = document.getElementById('res')
 
 function inserir(){ 
 let cidade = document.getElementById('cidade').value
-valor_cidade.push('cidade')
+valor_cidade.push(cidade)
 console.log(valor_cidade)
-
+res.innerHTML = valor_cidade
 }
 function excluir(){ 
-    let cidade = document.getElementById('cidade').value
-    valor_cidade.pop('cidade')
-    console.log(valor_cidade)
     
+    valor_cidade.pop()
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
     }
