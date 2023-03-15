bfbb5f604a3b0b5eae64ac3d7799cb99edebc8bf
ALEX BLASKOSKI <alex_blaskoski@estudante.sesisenai.org.br>
2023-03-15 10:28:51 -0300

    mais botoes adicionado junto de correção de bugs

--- vetor.js
+++ vetor.js
@@ -1,5 +1,4 @@
 let valor_cidade = []
-let res = document.getElementById('res')
 
 function inserir(){ 
 let cidade = document.getElementById('cidade').value
@@ -10,6 +9,19 @@ res.innerHTML = valor_cidade
 function excluir(){ 
     
     valor_cidade.pop()
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+}
+
+function inserir_inicio(){ 
+    let cidade = document.getElementById('cidade').value
+    valor_cidade.unshift(cidade)
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
+    }
+    
+function excluir_inicio(){ 
+    valor_cidade.shift(cidade)
     console.log(valor_cidade)
     res.innerHTML = valor_cidade
     }
